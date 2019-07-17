be7d33ffa8a1c04ca3ec2da0e6b2308bc75f859b
Long Dang <longdv2@vng.com.vn>
2019-07-17 22:56:12 +0700

    short declaration operator

--- code_sample/001-hello-world/main.go
+++ code_sample/001-hello-world/main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Hello folks")
+
+	foo()
+
+	fmt.Println("Something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+func bar() {
+	fmt.Println("and we exit")
 }
